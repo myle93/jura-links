7811163fd18f809cece5bd6e3311f4112c7c7b27
Ubuntu <emi.myle@protonmail.com>
2024-08-11 22:17:57 +0200

    disabled auto recognization; added instead new command

--- main.ts
+++ main.ts
@@ -4,31 +4,17 @@ export default class ExamplePlugin extends Plugin {
 
 	onload() {
 
-		// This event is triggered when document is changed
-		this.app.workspace.on("editor-change", (editor) => {
-			const content = editor.getDoc().getValue();
-			this.logRegexMatches(content, editor);
-		});
-		// This event is triggered when document is initially opened
-		this.app.workspace.on("active-leaf-change", () => {
-			this.readActiveFileAndUpdateLineCount();
+		this.addCommand({
+			id: 'apply',
+			name: 'apply',
+			editorCallback: (editor: Editor) => {
+				const content = editor.getDoc().getValue();
+				this.logRegexMatches(content, editor);
+			}
 		});
 	}
 
 	onunload() {
-	}
-
-	private async readActiveFileAndUpdateLineCount() {
-		const file = this.app.workspace.getActiveFile();
-		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-		const editor = view?.editor;
-
-		if (file) {
-			const content = await this.app.vault.read(file);
-			this.logRegexMatches(content, editor);
-		} else {
-			this.logRegexMatches(undefined, editor);
-		}
 	}
 
 	private logRegexMatches(fileContent?: string, editor?: Editor) {
@@ -42,11 +28,13 @@ export default class ExamplePlugin extends Plugin {
 		// 	4: 'nr',
 		// 	5: 'lit',
 		// 	6: 'gesetz',
-		var regex = /(§+|Art|Artikel)\.?\s*(\d+(?:\w\b)?)\s*(?:Abs\.\s*(\d+))?\s*(?:S\.\s*(\d+))?\s*(?:Nr\.\s*(\d+(?:\w\b)?))?\s*(?:lit\.\s*([a-z]?))?.{0,10}?(\b[A-Z][A-Za-z]*[A-Z](?:((?:\s|\b)[XIV]+)?))/gmi;
+		var regex = /(?<!<span class='law-article'>)(§+|Art|Artikel)\.?\s*(?<norm>\d+(?:\w\b)?)\s*(?:Abs\.\s*(?<absatz>\d+))?\s*(?:S\.\s*(?<satz>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]?))?.{0,10}?(?<gesetz>\b[A-Z][A-Za-z]*[A-Z](?:(?<buch>(?:\s|\b)[XIV]+)?))/gmi;
+
 		if (!regex.test(fileContent)) {
-			return
+		return
 		}
 		const url = 'https://www.gesetze-im-internet.de';
+		console.log('fileContent', fileContent);
 		fileContent = fileContent.replace(regex, `[<span class='law-article'>$&</span>](${url}/6/__2.html)`);
 		if (editor) {
 			editor.setValue(fileContent);
