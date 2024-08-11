8778adad33132be8bd9865dcd96bab2408f28e3d
Ubuntu <emi.myle@protonmail.com>
2024-08-11 22:18:12 +0200

    Fixed regex; fixed url

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Editor, MarkdownView, Plugin } from "obsidian";
+import { Editor, Plugin } from "obsidian";
 
 export default class ExamplePlugin extends Plugin {
 
@@ -21,21 +21,16 @@ export default class ExamplePlugin extends Plugin {
 		if (!fileContent) {
 			return;
 		}
-		// regex groups
-		// 	1 : 'norm',
-		// 	2: 'absatz',
-		// 	3: 'satz',
-		// 	4: 'nr',
-		// 	5: 'lit',
-		// 	6: 'gesetz',
-		var regex = /(?<!<span class='law-article'>)(§+|Art|Artikel)\.?\s*(?<norm>\d+(?:\w\b)?)\s*(?:Abs\.\s*(?<absatz>\d+))?\s*(?:S\.\s*(?<satz>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]?))?.{0,10}?(?<gesetz>\b[A-Z][A-Za-z]*[A-Z](?:(?<buch>(?:\s|\b)[XIV]+)?))/gmi;
+		var regex = /(?<!<span class='law-article'>)(?<art>§+|Art|Artikel)\.?\s*(?<norm>\d+(?:\w\b)?)\s*(?:Abs\.\s*(?<absatz>\d+))?\s*(?:S\.\s*(?<satz>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]?))?.{0,10}?(?<gesetz>\b[A-Z][A-Za-z]*[A-Z](?:(?<buch>(?:\s|\b)[XIV]+)?))/gm;
 
 		if (!regex.test(fileContent)) {
 		return
 		}
 		const url = 'https://www.gesetze-im-internet.de';
-		console.log('fileContent', fileContent);
-		fileContent = fileContent.replace(regex, `[<span class='law-article'>$&</span>](${url}/6/__2.html)`);
+		fileContent = fileContent.replace(regex, (match, art, norm, absatz, satz, nr, lit, gesetz, buch) => {
+			gesetz = gesetz.toLowerCase();
+			return `[<span class='law-article'>${match}</span>](${url}/${gesetz}/__${norm}.html)`;
+		});
 		if (editor) {
 			editor.setValue(fileContent);
 		}
