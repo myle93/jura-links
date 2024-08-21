e73dc27eb15d0452dc7666e39cf2e639391b2634
Ubuntu <emi.myle@protonmail.com>
2024-08-21 20:53:18 +0200

    Changed addCommand to auto recog; extended regex to recog for umlauts

--- main.ts
+++ main.ts
@@ -4,13 +4,14 @@ export default class ExamplePlugin extends Plugin {
 
 	onload() {
 
-		this.addCommand({
-			id: 'apply',
-			name: 'apply',
-			editorCallback: (editor: Editor) => {
-				const content = editor.getDoc().getValue();
-				this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
-			}
+		// This event is triggered when document is changed
+		this.app.workspace.on("editor-change", (editor) => {
+			const content = editor.getDoc().getValue();
+			this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
+		});
+		// This event is triggered when document is initially opened
+		this.app.workspace.on("active-leaf-change", () => {
+			this.findAndReplaceLawArticlesWithHyperLinkAndIframe();
 		});
 	}
 
@@ -21,12 +22,14 @@ export default class ExamplePlugin extends Plugin {
 		if (!fileContent) {
 			return;
 		}
-		var regex = /(?<!\.html">)(?<art>§+|Art|Artikel)\.?\s*(?<norm>\d+(?:\w\b)?)\s*(?:Abs\.\s*(?<absatz>\d+))?\s*(?:S\.\s*(?<satz>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]?))?.{0,10}?(?<gesetz>\b[A-Z][A-Za-z]*[A-Z](?:(?<buch>(?:\s|\b)[XIV]+)?))/gm;
-
-		if (!regex.test(fileContent)) {
-		return
-		}
-		if (!editor) {
+	
+		const lawRegex = /(?<!<a\s+href="[^"]{0,1000}">)(?<p1>§§?|Art\.?|Artikel)\s*(?<norm>(?:\d+(?:\w\b)?(?:\s*,\s*\d+(?:\w\b)?)*))(?:\s*(?:(?<absatz>[IVXLCDM]+|\d+))?\s*(?:(?<satz>\d+))?\s*(?:Alt\.\s*(?<alternative>\d+))?\s*(?:Var\.\s*(?<variante>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]))?)?\s*(?<gesetz>[A-Z][A-ZÄÖÜßa-zäöüß0-9-]*(?:-[IVX]+)?(?:-[A-Za-z0-9]+)*)\s*(?<buch>[IVX]+)?(?=\W|$)/gm;
+	
+		const caseRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?:[CTF]-\d+\/\d{2}|(?:[IVXLCDM]+\s*)?\d+\s*[A-Za-z]{1,3}\s*\d+\/\d{2}|\d{1,7}\/\d{2})\b/g;
+	
+		const journalRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?<journal>[A-Z]{2,4}(?:-[A-Z]{2,4})?)\s*(?<year>\d{4}),\s*(?<page>\d+)(?:\s*\(\d+\))?\b/gm;
+	
+		if (!lawRegex.test(fileContent) && !caseRegex.test(fileContent) && !journalRegex.test(fileContent)) {
 			return;
 		}
 		const url = 'https://www.gesetze-im-internet.de';
