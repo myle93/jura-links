ccbdf2b2899bda5fe3fca55307211f4287e04372
Ubuntu <emi.myle@protonmail.com>
2024-08-21 20:44:45 +0200

    Automatically recognize law articles; extended regex to recognize umlauts

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ This project is an Obsidian plugin called "Obsidian Law Article Highlighter". It
 - [x] Regex for law article recognition
 - [x] Highlight for law articles
 - [x] Hyperlink for law articles
-- [x] Preview of law articles with iframe
+- [x] RegEx muss auch Umlaute erkennen.
+  		- so gibt es zum Beispiel die EuGVÜ als EU-Verordnung. Hier erkennt der Code bisher nur bis zu "EuGV". Einen Code zur Umwandlung der Umlaute gibt es bereits, aber die RegEx muss sie auch erkennen.
+- [ ] Weitere Verfeinerung des RegEx für Gesetze, um auch Gesetzesbereiche wie §§ 358 - 360 BGB zu erkennen. Dabei soll die erste und die letzte Norm verlinkt werden.
+- [ ] Hyperlinks für jede Norm in einer Paragraphenkette die mit "§§" oder "Artt." beginnt (Beispiel: §§ 311 II, 280 I, 241 II BGB)
+- [ ] RegEx der Zeitschriften muss auch amtliche Zeitschriftensammlungen wie BGHZ 31, 242 erkennen. Diese Liste ist recht abschließend, sodass ich da auch eine vordefinierte Liste an solchen Zeitschriften geben kann, falls es das leichter macht.
+- [ ] Preview of law articles with small iframe
 - [ ] Generalize hyperlinks
+- [ ] Anzeigetext in den Hyperlinks bei Paragraphenketten gleicht der Eingabe, also §§ 311 II, [...] BGB wird eingegeben und 311 II ist auch Anzeigetext.
 - [ ] Tests
 - [ ] Refactor
--- main.ts
+++ main.ts
@@ -4,13 +4,14 @@ export default class ExamplePlugin extends Plugin {
 
 	onload() {
 
-		this.addCommand({
-			id: 'apply',
-			name: 'apply',
-			editorCallback: (editor: Editor) => {
-				const content = editor.getDoc().getValue();
-				this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
-			}
+		// This event is triggered when document is changed
+		this.app.workspace.on("editor-change", (editor) => {
+			const content = editor.getDoc().getValue();
+			this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
+		});
+		// This event is triggered when document is initially opened
+		this.app.workspace.on("active-leaf-change", () => {
+			this.findAndReplaceLawArticlesWithHyperLinkAndIframe();
 		});
 	}
 
@@ -18,24 +19,57 @@ export default class ExamplePlugin extends Plugin {
 	}
 
 	private findAndReplaceLawArticlesWithHyperLinkAndIframe(fileContent?: string, editor?: Editor) {
-		if (!fileContent) {
+		if (!fileContent || !editor) {
 			return;
 		}
-		var regex = /(?<!\.html">)(?<art>§+|Art|Artikel)\.?\s*(?<norm>\d+(?:\w\b)?)\s*(?:Abs\.\s*(?<absatz>\d+))?\s*(?:S\.\s*(?<satz>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]?))?.{0,10}?(?<gesetz>\b[A-Z][A-Za-z]*[A-Z](?:(?<buch>(?:\s|\b)[XIV]+)?))/gm;
-
-		if (!regex.test(fileContent)) {
-		return
-		}
-		if (!editor) {
+	
+		const lawRegex = /(?<!<a\s+href="[^"]{0,1000}">)(?<p1>§§?|Art\.?|Artikel)\s*(?<norm>(?:\d+(?:\w\b)?(?:\s*,\s*\d+(?:\w\b)?)*))(?:\s*(?:(?<absatz>[IVXLCDM]+|\d+))?\s*(?:(?<satz>\d+))?\s*(?:Alt\.\s*(?<alternative>\d+))?\s*(?:Var\.\s*(?<variante>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]))?)?\s*(?<gesetz>[A-Z][A-ZÄÖÜßa-zäöüß0-9-]*(?:-[IVX]+)?(?:-[A-Za-z0-9]+)*)\s*(?<buch>[IVX]+)?(?=\W|$)/gm;
+	
+		const caseRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?:[CTF]-\d+\/\d{2}|(?:[IVXLCDM]+\s*)?\d+\s*[A-Za-z]{1,3}\s*\d+\/\d{2}|\d{1,7}\/\d{2})\b/g;
+	
+		const journalRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?<journal>[A-Z]{2,4}(?:-[A-Z]{2,4})?)\s*(?<year>\d{4}),\s*(?<page>\d+)(?:\s*\(\d+\))?\b/gm;
+	
+		if (!lawRegex.test(fileContent) && !caseRegex.test(fileContent) && !journalRegex.test(fileContent)) {
 			return;
 		}
-		const url = 'https://www.gesetze-im-internet.de';
-		fileContent = fileContent.replace(regex, (match, art, norm, absatz, satz, nr, lit, gesetz, buch) => {
-			gesetz = gesetz.toLowerCase();
-			var artRegex = /(?<art>§+|Art|Artikel)/gm;
-			match = match.replace(artRegex, `<span class="law-article">${art}</span>`);
-			return `<a class="iframe-link law-article" href="${url}/${gesetz}/__${norm}.html">${match}<iframe src="${url}/${gesetz}/__${norm}.html"></iframe></a>`
+	
+		const lawUrl = 'https://www.dejure.org/gesetze';
+		const caseUrl = 'https://dejure.org/dienste/vernetzung/rechtsprechung?Text=';
+		const journalUrl = 'https://dejure.org/dienste/vernetzung/rechtsprechung?Text=';
+	
+		fileContent = fileContent.replace(lawRegex, (match, p1, norm, absatz, satz, alternative, variante, nr, lit, gesetz, buch) => {
+			console.log(match);
+			// Transform law name for the URL
+			gesetz = gesetz.toLowerCase()
+						   .replace(/ä/g, 'ae')
+						   .replace(/ö/g, 'oe')
+						   .replace(/ü/g, 'ue')
+						   .replace(/ß/g, 'ss');
+			
+			buch = buch ? `_${buch.toUpperCase()}` : '';
+	
+			// Check if it's a chain of laws (§§ or Artt.)
+			if (p1 === '§§' || p1 === 'Artt.') {
+				const normList = norm.split(',').map((n: string) => n.trim());
+				const links = normList.map((n: string, index: number) => {
+					return `<a href="${lawUrl}/${gesetz}${buch}/${n}.html">${index === 0 ? p1.charAt(0) : ''} ${n}</a>`;
+				}).join(', ');
+				
+				const extras = [absatz, satz, alternative, variante, nr, lit].filter(Boolean).join(' ');
+				return `${links}${extras ? ' ' + extras : ''} ${gesetz.toUpperCase()}`;
+			} else {
+				return `<a href="${lawUrl}/${gesetz}${buch}/${norm}.html">${match}</a>`;
+			}
 		});
-		editor.setValue(fileContent);
+	
+		fileContent = fileContent.replace(caseRegex, (match) => {
+			return `<a href="${caseUrl}${encodeURIComponent(match)}">${match}</a>`;
+		});
+	
+		fileContent = fileContent.replace(journalRegex, (match, journal, year, page) => {
+			return `<a href="${journalUrl}${encodeURIComponent(match)}">${match}</a>`;
+		});
+	
+		editor.getDoc().setValue(fileContent);
 	}
 }
