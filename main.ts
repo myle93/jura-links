69cb144c6e7bce69b49a5008405f41d828eb4fb6
Ubuntu <emi.myle@protonmail.com>
2024-08-21 20:53:18 +0200

    Changed addCommand to auto recog; extended regex to recog for umlauts

--- main.ts
+++ main.ts
@@ -4,13 +4,14 @@ export default class ExamplePlugin extends Plugin {
 
 	onload() {
 
-		this.addCommand({
-			id: 'apply',
-			name: 'apply',
-			editorCallback: (editor: Editor) => {
-				const content = editor.getDoc().getValue();
-				this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
-			}
+		// This event is triggered when document is changed
+		this.app.workspace.on("editor-change", (editor) => {
+			const content = editor.getDoc().getValue();
+			this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
+		});
+		// This event is triggered when document is initially opened
+		this.app.workspace.on("active-leaf-change", () => {
+			this.findAndReplaceLawArticlesWithHyperLinkAndIframe();
 		});
 	}
 
@@ -22,7 +23,7 @@ export default class ExamplePlugin extends Plugin {
 			return;
 		}
 	
-		const lawRegex = /(?<!<a\s+href="[^"]{0,1000}">)(?<p1>§§?|Art\.?|Artikel)\s*(?<norm>(?:\d+(?:\w\b)?(?:\s*,\s*\d+(?:\w\b)?)*))(?:\s*(?:(?<absatz>[IVXLCDM]+|\d+))?\s*(?:(?<satz>\d+))?\s*(?:Alt\.\s*(?<alternative>\d+))?\s*(?:Var\.\s*(?<variante>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]))?)?\s*(?<gesetz>[A-Z][A-Za-z0-9-]*(?:-[IVX]+)?(?:-[A-Za-z0-9]+)*)\s*(?<buch>[IVX]+)?(?=\W|$)/gm;
+		const lawRegex = /(?<!<a\s+href="[^"]{0,1000}">)(?<p1>§§?|Art\.?|Artikel)\s*(?<norm>(?:\d+(?:\w\b)?(?:\s*,\s*\d+(?:\w\b)?)*))(?:\s*(?:(?<absatz>[IVXLCDM]+|\d+))?\s*(?:(?<satz>\d+))?\s*(?:Alt\.\s*(?<alternative>\d+))?\s*(?:Var\.\s*(?<variante>\d+))?\s*(?:Nr\.\s*(?<nr>\d+(?:\w\b)?))?\s*(?:lit\.\s*(?<lit>[a-z]))?)?\s*(?<gesetz>[A-Z][A-ZÄÖÜßa-zäöüß0-9-]*(?:-[IVX]+)?(?:-[A-Za-z0-9]+)*)\s*(?<buch>[IVX]+)?(?=\W|$)/gm;
 	
 		const caseRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?:[CTF]-\d+\/\d{2}|(?:[IVXLCDM]+\s*)?\d+\s*[A-Za-z]{1,3}\s*\d+\/\d{2}|\d{1,7}\/\d{2})\b/g;
 	
