6f8066f75d3ffe86b9350578075eac3ab924f4ed
Ubuntu <emi.myle@protonmail.com>
2024-08-23 18:38:52 +0200

    Rm event on editor change; readded command; fixed regex

--- main.ts
+++ main.ts
@@ -3,27 +3,30 @@ import { Editor, Plugin } from "obsidian";
 export default class ExamplePlugin extends Plugin {
 
 	onload() {
-
-		// This event is triggered when document is changed
-		this.app.workspace.on("editor-change", (editor) => {
-			const content = editor.getDoc().getValue();
-			this.findAndReplaceLawArticlesWithHyperLinkAndIframe(content, editor);
-		});
 		// This event is triggered when document is initially opened
 		this.app.workspace.on("active-leaf-change", () => {
-			this.findAndReplaceLawArticlesWithHyperLinkAndIframe();
+			this.findAndLinkLawArticles();
+		});
+
+		this.addCommand({
+			id: 'apply',
+			name: 'apply',
+			editorCallback: (editor: Editor) => {
+				const content = editor.getDoc().getValue();
+				this.findAndLinkLawArticles(content, editor);
+			}
 		});
 	}
 
 	onunload() {
 	}
 
-	private findAndReplaceLawArticlesWithHyperLinkAndIframe(fileContent?: string, editor?: Editor) {
+	private findAndLinkLawArticles(fileContent?: string, editor?: Editor) {
 		if (!fileContent || !editor) {
 			return;
 		}
 	
-		const lawRegex = /(?<!\.html">)(?<p1>§+|Art\.|Artikel)\s*(?<p2>(?<norm>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*)\s*(?:(Abs\.|Absatz)\s*(?<absatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<absatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<satz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternative)\s*(?<alternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<variante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<nr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<lit>[a-z][a-z-]*[a-z]?))?.{0,10}?(?:\s*(,|-|und)\s*(?<lnorm>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*)\s*(?:(Abs\.|Absatz)\s*(?<labsatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<labsatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<lsatz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternative)\s*(?<lalternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<lvariante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<lnr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<llit>[a-z][a-z-]*[a-z]?))?.{0,10}?)*)(?<gesetz>\b[A-ZÄÖÜß][A-ZÄÖÜẞa-zäöüß-]*[A-ZÄÖÜß])\s*(?<buch>[IVX]+)?/gm;
+		const lawRegex = /(?<!\.html">)(?<p1>§+|Art\.|Artikel)\s*(?<p2>(?<norm>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*)\s*(?:(Abs\.|Absatz)\s*(?<absatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<absatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<satz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<alternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<variante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<nr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<lit>[a-z][a-z-]*[a-z]?))?.{0,10}?(?:\s*(,|-|und)\s*(?<lnorm>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*)\s*(?:(Abs\.|Absatz)\s*(?<labsatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<labsatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<lsatz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<lalternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<lvariante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<lnr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<llit>[a-z][a-z-]*[a-z]?))?.{0,10}?)*)(?<gesetz>(SGB\s*(?<buch>[IVX]+)|\b[A-ZÄÖÜß][A-ZÄÖÜẞa-zäöüß-]*[A-ZÄÖÜß]))/gm;
 	
 		const caseRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?:[CTF]-\d+\/\d{2}|(?:[IVXLCDM]+\s*)?\d+\s*[A-Za-z]{1,3}\s*\d+\/\d{2}|\d{1,7}\/\d{2})\b/g;
 	
@@ -37,7 +40,7 @@ export default class ExamplePlugin extends Plugin {
 		const caseUrl = 'https://dejure.org/dienste/vernetzung/rechtsprechung?Text=';
 		const journalUrl = 'https://dejure.org/dienste/vernetzung/rechtsprechung?Text=';
 	
-		fileContent = fileContent.replace(lawRegex, (match, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, groups) => {
+		fileContent = fileContent.replace(lawRegex, (match, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, groups) => {
 
 			// Transform law name for the URL
 			const gesetz = groups.gesetz.toLowerCase()
