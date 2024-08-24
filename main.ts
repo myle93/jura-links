acf87309b7c0c9a7dc74798170caf49a2d3b2eb7
Ubuntu <emi.myle@protonmail.com>
2024-08-24 10:36:34 +0200

    Link the whole norm group together with its abs, s....

--- main.ts
+++ main.ts
@@ -52,6 +52,7 @@ export default class ExamplePlugin extends Plugin {
 		// So if the previous line is not empty, return.
 		// Else edit the current line.
 		const previousLineContent = editor.getLine(editor.lastLine()-1)
+		console.log(previousLineContent);
 		if (previousLineContent.trim().length > 0) {
 			return;
 		}
@@ -69,9 +70,9 @@ export default class ExamplePlugin extends Plugin {
 
 	private findAndLinkLegalArticles(fileContent: string){
 		
-		const lawRegex = /(?<!html">)(?<p1>§+|Art\.|Artikel)\s*(?<p2>(?<norm>\d+(?:\w\b)?)\s*(?:(Abs\.|Absatz)\s*(?<absatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<absatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<satz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<alternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<variante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<nr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<lit>[a-z][a-z-]*[a-z]?))?.{0,10}?(?:\s*(,|-|und)\s*(?<lnorm>\d+(?:\w\b)?)\s*(?:(Abs\.|Absatz)\s*(?<labsatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<labsatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<lsatz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<lalternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<lvariante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<lnr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<llit>[a-z][a-z-]*[a-z]?))?.{0,10}?)*)(?<gesetz>(SGB\s*(?<buch>[IVX]+)|\b[A-ZÄÖÜß][A-ZÄÖÜẞa-zäöüß-]*[A-ZÄÖÜß]))/gm;
+		const lawRegex = /(?<!html">)(?<p1>§+|Art\.|Artikel)\s*(?<p2>(?<normgr>(?<norm>\d+(?:\w\b)?)\s*(?:(Abs\.|Absatz)\s*(?<absatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<absatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<satz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<alternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<variante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<nr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<lit>[a-z][a-z-]*[a-z]?))?.{0,10}?)(?:\s*(,|-|und)\s*(?<lnorm>\d+(?:\w\b)?)\s*(?:(Abs\.|Absatz)\s*(?<labsatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<labsatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<lsatz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<lalternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<lvariante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<lnr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<llit>[a-z][a-z-]*[a-z]?))?.{0,10}?)*)(?<gesetz>(SGB\s*(?<buch>[IVX]+)|\b[A-ZÄÖÜß][A-ZÄÖÜẞa-zäöüß-]*[A-ZÄÖÜß]))/gm;
 
-		const lawChainRegex = /(?:\s*(,|-|und)\s*)(?<norm>\d+(?:\w\b)?)\s*(?:(Abs\.|Absatz)\s*(?<absatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<absatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<satz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<alternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<variante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<nr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<lit>[a-z][a-z-]*[a-z]?))?.{0,10}?/gm;
+		const lawChainRegex = /(?:\s*(,|-|und)\s*)(?<normgr>(?<norm>\d+(?:\w\b)?)\s*(?:(Abs\.|Absatz)\s*(?<absatz>\d+(?:\s*(,|-|und)\s*\d+)*)|(?<absatzrom>[IVXLCDM]+(?:\s*(,|-|und)\s*[IVXLCDM]+)*))?\s*(?:(S\.|Satz)?\s*(?<satz>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Alt\.|Alternativ)\s*(?<alternative>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Var\.|Variante)\s*(?<variante>\d+(?:\s*(,|-|und)\s*\d+)*))?\s*(?:(Nr\.|Nummer)\s*(?<nr>\d+(?:\w\b)?(?:\s*(,|-|und)\s*\d+(?:\w\b)?)*))?\s*(?:(lit\.|Buchstabe)\s*(?<lit>[a-z][a-z-]*[a-z]?))?.{0,10}?)/gm;
 	
 		const caseRegex = /(?<!<a\s+href="[^"]{0,1000}">)\b(?:[CTF]-\d+\/\d{2}|(?:[IVXLCDM]+\s*)?\d+\s*[A-Za-z]{1,3}\s*\d+\/\d{2}|\d{1,7}\/\d{2})\b/g;
 	
@@ -85,7 +86,7 @@ export default class ExamplePlugin extends Plugin {
 		const caseUrl = 'https://dejure.org/dienste/vernetzung/rechtsprechung?Text=';
 		const journalUrl = 'https://dejure.org/dienste/vernetzung/rechtsprechung?Text=';
 	
-		fileContent = fileContent.replace(lawRegex, (match, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, groups) => {
+		fileContent = fileContent.replace(lawRegex, (match, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, groups) => {
 
 			// Transform law name for the URL
 			const gesetz = groups.gesetz.toLowerCase()
@@ -107,17 +108,19 @@ export default class ExamplePlugin extends Plugin {
 			let lawMatch: string = groups.p2;
 
 			// fistNorm: 23
-			const firstNorm = groups.norm;
-			const firstNormLinks = `<a class="no-underline" href="${lawUrl}/${gesetz}${buch}/${firstNorm}.html">${firstNorm}</a>`;
-			lawMatch = lawMatch.replace(firstNorm, firstNormLinks);
+			const firstNormGroup = groups.normgr;
+			const firstNorm = firstNormGroup.norm;
+			const firstNormLinks = `<a class="no-underline" href="${lawUrl}/${gesetz}${buch}/${firstNorm}.html">${firstNormGroup}</a>`;
+			lawMatch = lawMatch.replace(firstNormGroup, firstNormLinks);
 
 			const gp1 = groups.p1;
 			if(/§§+/.test(gp1)) {
 				// matches of lawRegexChain: ["", 24 II", ", 25 II"]
-				lawMatch = lawMatch.replace(lawChainRegex, (match, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, groups) => {
+				lawMatch = lawMatch.replace(lawChainRegex, (match, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, groups) => {
 					const norm = groups.norm;
-					const normLink = `<a class="no-underline" href="${lawUrl}/${gesetz}${buch}/${norm}.html">${norm}</a>`;
-					match = match.replace(norm, normLink);
+					const normGroup = groups.normgr;
+					const normLink = `<a class="no-underline" href="${lawUrl}/${gesetz}${buch}/${norm}.html">${normGroup}</a>`;
+					match = match.replace(normGroup, normLink);
 					return match;
 				});
 			}
