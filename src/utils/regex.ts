d43dd33ee83ecb95653a3271e5e015d8919dc229
Lukas Collier <129078768+justanotherjurastudent@users.noreply.github.com>
2024-09-24 16:47:49 +0200

    Verbinder angepasst

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,24 +2,25 @@ import { AllLawAbbrs } from "../static/lawsAbbrs";
 
 const lawList = AllLawAbbrs.join("|");
 
-const Verbinder = "(?:\\s*(-|und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
-const VerbinderKomma = "(?:\\s*(,|-|und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
+const Verbinder = "(?:\\s*(und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
+const VerbinderKomma = "(?:\\s*(,|und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
+const VerbinderGesetz = "(?:\\s*(,|-|bis)\\s*";
 
 function getSingleLawRegexString(suffix: string): string {
-    return `(?<normgr${suffix}>(?<norm${suffix}>\\d+(?:\\w\\b)?)\\s*(?:(Abs\\.|Absatz)\\s*(?<absatz${suffix}>\\d+${Verbinder}\\d+)*)|(?<absatzrom${suffix}>[IVXLCDM]+${VerbinderKomma}[IVXLCDM]+)*))?\\s*(?:(S\\.|Satz)?\\s*(?<satz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Hs\\.|Halbsatz)\\s*(?<halbsatz{suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Var\\.|Variante)\\s*(?<variante${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Nr\\.|Nummer)\\s*(?<nr${suffix}>\\d+(?:\\w\\b)?${Verbinder}\\d+(?:\\w\\b)?)*))?\\s*(?:(lit\\.|Buchstabe)\\s*(?<lit${suffix}>[a-z]${Verbinder}[a-z])*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative2${suffix}>\\d+${Verbinder}\\d+)?))?.{0,10}?)`;
+    return `(?<normgr${suffix}>(?<norm${suffix}>\\d+(?:\\w\\b)?)\\s*(?:(Abs\\.|Absatz)\\s*(?<absatz${suffix}>\\d+${Verbinder}\\d+)*)|(?<absatzrom${suffix}>[IVXLCDM]+${VerbinderKomma}[IVXLCDM]+)*))?\\s*(?:(S\\.|Satz)?\\s*(?<satz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Hs\\.|Halbsatz)\\s*(?<halbsatz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Var\\.|Variante)\\s*(?<variante${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Nr\\.|Nummer)\\s*(?<nr${suffix}>\\d+(?:\\w\\b)?${Verbinder}\\d+(?:\\w\\b)?)*))?\\s*(?:(lit\\.|Buchstabe)\\s*(?<lit${suffix}>[a-z]${Verbinder}[a-z])*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative2${suffix}>\\d+${Verbinder}\\d+)?))?.{0,10}?)`;
 }
 
 export const lawRegex = new RegExp(
     `(?<!\[[^\]]{0,1})(?<p1>§+|Art\\.|Artikel)\\s*(?<p2>${getSingleLawRegexString(
         "_first"
-    )}${VerbinderKomma}${getSingleLawRegexString(
+    )}${VerbinderGesetz}${getSingleLawRegexString(
         "_last"
     )})*)(?<gesetz>\\b${lawList}\\b)(?!.*?\\])`,
     "gm"
 );
 
 export const lawChainRegex = new RegExp(
-    `${VerbinderKomma})${getSingleLawRegexString("")}`,
+    `${VerbinderGesetz})${getSingleLawRegexString("")}`,
     "gm"
 );
 
