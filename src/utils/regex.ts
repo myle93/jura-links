c152c803a1c51e4ce7fcad4685a62fedd0ebd140
Ubuntu <emi.myle@protonmail.com>
2024-09-25 14:51:40 +0200

    Fixed bug in replacing last norm

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,27 +3,30 @@ import { AllLawAbbrs } from "../static/lawsAbbrs";
 const lawList = AllLawAbbrs.join("|");
 
 const Verbinder = "(?:\\s*(und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
-const VerbinderKomma = "(?:\\s*(,|und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
+const VerbinderKomma =
+	"(?:\\s*(,|und|u\\.|iVm|i\\.V\\.m\\.|i\\. V\\. m\\.)\\s*";
 const VerbinderGesetz = "(?:\\s*(,|-|bis)\\s*";
 
 function getSingleLawRegexString(suffix: string): string {
-    return `(?<normgr${suffix}>(?<norm${suffix}>\\d+(?:\\w\\b)?)\\s*(?:(Abs\\.|Absatz)\\s*(?<absatz${suffix}>\\d+${Verbinder}\\d+)*)|(?<absatzrom${suffix}>[IVXLCDM]+${VerbinderKomma}[IVXLCDM]+)*))?\\s*(?:(S\\.|Satz)?\\s*(?<satz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Hs\\.|Halbsatz)\\s*(?<halbsatz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Var\\.|Variante)\\s*(?<variante${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Nr\\.|Nummer)\\s*(?<nr${suffix}>\\d+(?:\\w\\b)?${Verbinder}\\d+(?:\\w\\b)?)*))?\\s*(?:(lit\\.|Buchstabe)\\s*(?<lit${suffix}>[a-z]${Verbinder}[a-z])*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative2${suffix}>\\d+${Verbinder}\\d+)?))?.{0,10}?)`;
+	return `(?<normgr${suffix}>(?<norm${suffix}>\\d+(?:\\w\\b)?)\\s*(?:(Abs\\.|Absatz)\\s*(?<absatz${suffix}>\\d+${Verbinder}\\d+)*)|(?<absatzrom${suffix}>[IVXLCDM]+${VerbinderKomma}[IVXLCDM]+)*))?\\s*(?:(S\\.|Satz)?\\s*(?<satz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Hs\\.|Halbsatz)\\s*(?<halbsatz${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Var\\.|Variante)\\s*(?<variante${suffix}>\\d+${Verbinder}\\d+)*))?\\s*(?:(Nr\\.|Nummer)\\s*(?<nr${suffix}>\\d+(?:\\w\\b)?${Verbinder}\\d+(?:\\w\\b)?)*))?\\s*(?:(lit\\.|Buchstabe)\\s*(?<lit${suffix}>[a-z]${Verbinder}[a-z])*))?\\s*(?:(Alt\\.|Alternative)\\s*(?<alternative2${suffix}>\\d+${Verbinder}\\d+)?))?.{0,10}?)`;
 }
 
 export const lawRegex = new RegExp(
-    `(?<!\[[^\]]{0,1})(?<p1>§+|Art\\.|Artikel)\\s*(?<p2>${getSingleLawRegexString(
-        "_first"
-    )}${VerbinderGesetz}${getSingleLawRegexString(
-        "_last"
-    )})*)(?<gesetz>\\b${lawList}\\b)(?!.*?\\])`,
-    "gm"
+	`(?<!\[[^\]]{0,1})(?<p1>§+|Art\\.|Artikel)\\s*(?<p2>${getSingleLawRegexString(
+		"_first"
+	)}${VerbinderGesetz}${getSingleLawRegexString(
+		"_last"
+	)})*)(?<gesetz>\\b${lawList}\\b)(?!.*?\\])`,
+	"gm"
 );
 
 export const lawChainRegex = new RegExp(
-    `${VerbinderGesetz})${getSingleLawRegexString("")}`,
-    "gm"
+	`${VerbinderGesetz})${getSingleLawRegexString("")}`,
+	"gm"
 );
 
-export const caseRegex = /(?<!\[[^\]]{0,1})\b(?:[A-Za-z]-\d+\/\d{2}|[A-Za-z]\s*\d+\s*[A-Za-z]{1,3}\s*\d+\s*[A-Za-z]{0,3}\s*\d+\/\d{2}(?:\s*[A-Za-z])?|[A-Z]-\d+\/\d{2}|\d+[A-Za-z]?\s*[A-Za-z]{1,3}\s*\d+\s*[A-Za-z]{0,3}\s*\d+\/\d{2}(?:\s*[A-Za-z])?|[IVXLCDM]+\s*[A-Za-z]{1,3}\s*\d+\s*[A-Za-z]{0,3}\s*\d+\/\d{2}(?:\s*[A-Za-z])?|[A-Za-z]?\d{1,7}\/\d{2})\b(?!.*?\])/g;
+export const caseRegex =
+	/(?<!\[[^\]]{0,1})\b(?:[A-Za-z]-\d+\/\d{2}|[A-Za-z]\s*\d+\s*[A-Za-z]{1,3}\s*\d+\s*[A-Za-z]{0,3}\s*\d+\/\d{2}(?:\s*[A-Za-z])?|[A-Z]-\d+\/\d{2}|\d+[A-Za-z]?\s*[A-Za-z]{1,3}\s*\d+\s*[A-Za-z]{0,3}\s*\d+\/\d{2}(?:\s*[A-Za-z])?|[IVXLCDM]+\s*[A-Za-z]{1,3}\s*\d+\s*[A-Za-z]{0,3}\s*\d+\/\d{2}(?:\s*[A-Za-z])?|[A-Za-z]?\d{1,7}\/\d{2})\b(?!.*?\])/g;
 
-export const journalRegex = /(?<!\[[^\]]{0,1})\b(?<journal>[A-Za-z][A-Za-z-]*|Slg.)\s*(?:(?<year>\d{4})(?:,\s*(?<volume1>[IVXLCDM]{1,5})-(?<page1>\d+)|,\s*(?<page2>\d+))|(?<volume2>\d+),\s*(?<page3>\d+))\b(?!.*?\])/gm;
+export const journalRegex =
+	/(?<!\[[^\]]{0,1})\b(?<journal>[A-Za-z][A-Za-z-]*|Slg.)\s*(?:(?<year>\d{4})(?:,\s*(?<volume1>[IVXLCDM]{1,5})-(?<page1>\d+)|,\s*(?<page2>\d+))|(?<volume2>\d+),\s*(?<page3>\d+))\b(?!.*?\])/gm;
--- src/utils/transformation.ts
+++ src/utils/transformation.ts
@@ -4,81 +4,116 @@ import { caseRegex, journalRegex, lawChainRegex, lawRegex } from "./regex";
 import { getLawUrlByProviderOptions } from "./urlHelper";
 
 function findAndLinkLawReferences(
-    fileContent: string,
-    lawProviderOptions: LawProviderOptions = {
-        firstOption: "dejure",
-        secondOption: "lexsoft",
-        thirdOption: "lexmea",
-        forthOption: "buzer",
-        fifthOption: "rewis",
-    }
+	fileContent: string,
+	lawProviderOptions: LawProviderOptions = {
+		firstOption: "dejure",
+		secondOption: "lexsoft",
+		thirdOption: "lexmea",
+		forthOption: "buzer",
+		fifthOption: "rewis",
+	}
 ): string {
-    if (!lawRegex.test(fileContent)) {
-        return fileContent;
-    }
+	if (!lawRegex.test(fileContent)) {
+		return fileContent;
+	}
 
-    return fileContent.replace(lawRegex, (match, ...args) => {
-        const groups = args[args.length - 1];
-        let gesetz = groups.gesetz.trim().toLowerCase();
-        gesetz = gesetz === "brüssel-ia-vo" ? "eugvvo" : gesetz;
+	return fileContent.replace(lawRegex, (match, ...args) => {
+		const groups = args[args.length - 1];
+		let gesetz = groups.gesetz.trim().toLowerCase();
+		gesetz = gesetz === "brüssel-ia-vo" ? "eugvvo" : gesetz;
 
-        let lawMatch = groups.p2;
+		let lawMatch = groups.p2;
 
-        // Process first norm
-        const firstNormGroup = groups.normgr_first.trim();
-        const firstNorm = groups.norm_first;
-        const firstNormLink = getHyperlinkForLawIfExists(firstNormGroup, gesetz, firstNorm, lawProviderOptions);
-        lawMatch = lawMatch.replace(firstNormGroup, firstNormLink);
+		// Process first norm
+		const firstNormGroup = groups.normgr_first.trim();
+		const firstNorm = groups.norm_first;
+		const firstNormLink = getHyperlinkForLawIfExists(
+			firstNormGroup,
+			gesetz,
+			firstNorm,
+			lawProviderOptions
+		);
+		lawMatch = lawMatch.replace(firstNormGroup, firstNormLink);
 
-        // Process last norm if exists
-        if (groups.norm_last && groups.normgr_last) {
-            const lastNormGroup = groups.normgr_last.trim();
-            const lastNorm = groups.norm_last.trim();
-            const lastNormLink = getHyperlinkForLawIfExists(lastNormGroup, gesetz, lastNorm, lawProviderOptions);
-            lawMatch = lawMatch.replace(lastNormGroup, lastNormLink);
-        }
+		// Process last norm if exists
+		if (groups.norm_last && groups.normgr_last) {
+			const lastNormGroup = groups.normgr_last.trim();
+			const lastNorm = groups.norm_last.trim();
+			const lastNormLink = getHyperlinkForLawIfExists(
+				lastNormGroup,
+				gesetz,
+				lastNorm,
+				lawProviderOptions
+			);
+			lawMatch = replaceLast(lastNormGroup, lastNormLink, lawMatch);
+		}
 
-        // Process chain of laws
-        if (groups.p1 !== "§") {
-            lawMatch = lawMatch.replace(lawChainRegex, (chainMatch: string, ...chainArgs: (string | any)[]) => {
-                const chainGroups = chainArgs[chainArgs.length - 1];
-                const norm = chainGroups.norm.trim();
-                const normGroup = chainGroups.normgr.trim();
-                const normLink = getHyperlinkForLawIfExists(normGroup, gesetz, norm, lawProviderOptions);
-                return chainMatch.replace(normGroup, normLink);
-            });
-        }
+		// Process chain of laws
+		if (groups.p1 !== "§") {
+			lawMatch = lawMatch.replace(
+				lawChainRegex,
+				(chainMatch: string, ...chainArgs: (string | any)[]) => {
+					const chainGroups = chainArgs[chainArgs.length - 1];
+					const norm = chainGroups.norm.trim();
+					const normGroup = chainGroups.normgr.trim();
+					const normLink = getHyperlinkForLawIfExists(
+						normGroup,
+						gesetz,
+						norm,
+						lawProviderOptions
+					);
+					return chainMatch.replace(normGroup, normLink);
+				}
+			);
+		}
 
-        return match.replace(groups.p2, lawMatch);
-    });
+		return match.replace(groups.p2, lawMatch);
+	});
 }
 
 function findAndLinkCaseReferences(fileContent: string): string {
-    return fileContent.replace(caseRegex, match => {
-        const encodedMatch = encodeURIComponent(match);
-        return `[${match}](${DejureUrl.CASE}${encodedMatch})`;
-    });
+	return fileContent.replace(caseRegex, (match) => {
+		const encodedMatch = encodeURIComponent(match);
+		return `[${match}](${DejureUrl.CASE}${encodedMatch})`;
+	});
 }
 
 function findAndLinkJournalReferences(fileContent: string): string {
-    return fileContent.replace(journalRegex, match => {
-        const encodedMatch = encodeURIComponent(match);
-        return `[${match}](${DejureUrl.JOURNAL}${encodedMatch})`;
-    });
+	return fileContent.replace(journalRegex, (match) => {
+		const encodedMatch = encodeURIComponent(match);
+		return `[${match}](${DejureUrl.JOURNAL}${encodedMatch})`;
+	});
 }
 
 function getHyperlinkForLawIfExists(
-    normGroup: string,
-    gesetz: string,
-    norm: string,
-    lawProviderOptions: LawProviderOptions
+	normGroup: string,
+	gesetz: string,
+	norm: string,
+	lawProviderOptions: LawProviderOptions
 ): string {
-    const lawUrl = getLawUrlByProviderOptions(gesetz, norm, lawProviderOptions);
-    return lawUrl ? `[${normGroup}](${lawUrl})` : normGroup;
+	const lawUrl = getLawUrlByProviderOptions(gesetz, norm, lawProviderOptions);
+	return lawUrl ? `[${normGroup}](${lawUrl})` : normGroup;
+}
+
+function replaceLast(
+	oldString: string,
+	newString: string,
+	string: string
+): string {
+	var lastIndex = string.lastIndexOf(oldString);
+
+	if (lastIndex === -1) {
+		return string;
+	}
+
+	var beginString = string.substring(0, lastIndex);
+	var endString = string.substring(lastIndex + oldString.length);
+
+	return beginString + newString + endString;
 }
 
 export {
-    findAndLinkLawReferences,
-    findAndLinkCaseReferences,
-    findAndLinkJournalReferences,
+	findAndLinkLawReferences,
+	findAndLinkCaseReferences,
+	findAndLinkJournalReferences,
 };
